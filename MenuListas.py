0eeaef49041b8aff0693cc91470fa3129909c8c1
Azviole48 <astrid2violeta@hotmail.com>
2020-10-21 21:33:09 -0500

    Cambio de menulista

--- MenuListas.py
+++ MenuListas.py
@@ -1,24 +1,25 @@
 menu="""
- MENU    (CRUD)
- 1 ALTAS  (C)
- 2BAJAS   (D)
- 3CONSULTAS (R)
- 4MODIFICACIONES  (U)
- 5REPORTES   (R)
- 6SALIR
- 
-#print(menu)
-"""
+  MENU    (CRUD)
+  1 ALTAS  (C)
+  2BAJAS   (D)
+  3CONSULTAS (R)
+  4MODIFICACIONES  (U)
+  5REPORTES   (R)
+  6SALIR 
+  #print(menu)
+  """
 opc = None
+numCta=[]
 nombres=[]
 edades=[]
 carreras=[]
 promedios=[]
 while opc !=6:
     print(menu)
-    opc=int(input('dame una opcion'))
+    opc=int(input('dame una opcion:'))
 
     if opc == 1:
+        numCta.append(int(len(numCta)+1))
         nombres.append(input('Nombre:'))
         edades.append(int(input('Edad:')))
         carreras.append(input('Carrera:'))
@@ -28,19 +29,28 @@ while opc !=6:
     elif opc ==2:
         pass
     elif opc ==3:
-        pass
-    elif opc ==4:
-        pass
+        nC=int(input('Id a Consultar:'))
+        existe=False
+
+        for i in range (0,len(numCta)):
+            if numCta[i]==nC:
+                existe=True
+                break
+
+        if existe:
+            print('{:<5d}{:<20s}{:<5d}{:<10s}{:<5.2f}'.format(numCta[i]nombres[i],edades[i],carreras[i],promedios[i]))
+        else:
+            print(nC,'No existe')
+               
     elif opc ==4:
         pass
     elif opc ==5:
-        pass
-    elif opc ==5:
-        for i in range(0,len(alumnos)):
-           print('{:<10s}{:<5d}{:<10s}{:<5.2f}'.format(
-           nombres[i],edades[i],carreras[i],pormedios[i]))
+        for i in range(0,len(nombres)):
+           #print('%-10s%-5d%-10s%-5.2f'%(nombres[1],,edades[i],carreras[i],pormedios[i]))
+           print('{:<5d}{:<20s}{:<5d}{:<10s}{:<5.2f}'.format(numCta[i],nombres[i],edades[i],carreras[i],promedios[i]))
     elif opc ==6:
         pass
     else:
         print('opcion no valida')
+    input()
 

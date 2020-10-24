e57ec206ed82b38ebe59430c6c6a60ae6ec3d5c2
Azviole48 <astrid2violeta@hotmail.com>
2020-10-23 20:37:37 -0500

    dicionario

--- MenuDiccionarios.py
+++ MenuDiccionarios.py
@@ -10,58 +10,56 @@ menu="""
     """
 
 opc = None
-numCta=[]
-nombres=[]
-edades=[]
-carreras=[]
-promedios=[]
+alumnos=[]
 while opc != 6:
     os.system('cls')
     print(menu)
     opc = int(input('Dame una opción:'))
     
     if opc == 1:
-        numCta.append(int(len(numCta)+1))
-        nombres.append(input('Nombre:'))
-        edades.append(int(input('Edad:')))
-        carreras.append(input('Carrera:'))
-        promedios.append(float(input('Promedio:')))
+        a={}
+        a['numCta']=int(len(alumnos)+1)
+        a['nombre']=input('Nombre:')
+        a['edad']=int(input('Edad:'))
+        a['carrera']=input('Carrera:')
+        a['promedio']=float(input('Promedio:'))
+        alumnos.append(a)
+
         #Datos agregados en listas
     elif opc ==2:
        nC=int(input('Id a dar de baja:'))
-       existe = False
-       for i in range(0,len(numCta)):
-            if numCta[i]==nC:
+       existe=False
+       indice=-1
+       for i in alumnos:
+            indice+=1
+            if a['numCta']==nC:
                 existe=True
                 break
+           
         
        if existe:
-            numCta.pop(i)
-            nombres.pop(i)
-            edades.pop(i)
-            carreras.pop(i)
-            promedios.pop(i)
+            alumnos.pop(indice)
        else:
             print(nC,'No existe')
     elif opc ==3:
         nC=int(input('Id a Consultar:'))
         existe=False
-        for i in range(0,len(numCta)):
-            if numCta[i]==nC:
+        for i in alumnos:
+            if i['numCta']==nC:
                 existe=True
                 break
         
         if existe:
              print('{:<5d}{:<20s}{:<5d}{:<10s}{:<5.2f}'.format(
-            numCta[i],nombres[i],edades[i],carreras[i],promedios[i]))
+            i['numCta'],i['nombre'], i['edad'], i['carrera'], i['promedio']))
         else:
             print(nC,'No existe')
 
     elif opc ==4:
         nC=int(input('Id a Actualizar:'))
         existe=False
-        for i in range(0,len(numCta)):
-            if numCta[i]==nC:
+        for i in alumnos:
+            if i['numCta']==nC:
                 existe=True
                 break
         
@@ -81,13 +79,13 @@ while opc != 6:
                 opc2 = int(input('Dame una opción:'))
                 
                 if opc2 == 1:
-                   nombres[i]=input('dame el nuevo nombre:')
+                   i['nombre']=input('dame el nuevo nombre:')
                 elif opc2 ==2:
-                    edades[i]=int(input('dame la nueva edad:'))
+                    i['edades']=int(input('dame la nueva edad:'))
                 elif opc2==3:
-                    carreras[i]=input('dame la nueva carrera:')
+                    i['carreras']=input('dame la nueva carrera:')
                 elif opc2==4:
-                    promedios[i]=float(input('dame el nuevo promedio:'))
+                    i['promedios']=float(input('dame el nuevo promedio:'))
                 else:
                     print('regresando...')
                
@@ -98,10 +96,10 @@ while opc != 6:
 
 
     elif opc ==5:
-        for i in range(0,len(nombres)):
+        for i in alumnos:
             #print('%-20s%-5d%-10s%-5.2f'%(nombres[i],edades[i],carreras[i],promedios[i]))
             print('{:<5d}{:<20s}{:<5d}{:<10s}{:<5.2f}'.format(
-            numCta[i],nombres[i],edades[i],carreras[i],promedios[i]))
+            i['numCta'],i['nombre'], i['edad'], i['carrera'], i['promedio']))
     elif opc ==6:
         print('regrese pronto')
     else:
